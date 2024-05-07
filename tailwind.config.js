692f14112a088f76fb05805a47c6f26ef11263f5
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-05-07 20:46:36 +0330

    create coustom style first

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily:{
+          "dana-medium" : "dana-medium",
+          "dana-bold" :"dana-bold",
+          "dana-black": "dana-black"
+        }
+    },
+    screens: {
+      'sm': '576px',
+
+      'md': '768px',
+
+      'lg': '992px',
+
+      'xl': '1140px',
+    },
+    container:{
+      "center" : true ,
+     }
   },
   plugins: [],
 }
